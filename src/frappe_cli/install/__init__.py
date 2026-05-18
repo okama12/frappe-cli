9d03073403083489054a44a7012a706683b1427b
okama12 <rashidokama@gmail.com>
2026-05-19 00:45:47 +0200

    chore: bump version to 0.1.7 in pyproject.toml, rename wizard command to wizard_cmd for clarity

--- src/frappe_cli/install/__init__.py
+++ src/frappe_cli/install/__init__.py
@@ -11,7 +11,7 @@ from .prod import prod
 from .ssh_hardening import ssh_hardening
 from .system import system
 from .user import user
-from .wizard import wizard
+from .wizard import wizard as wizard_cmd
 
 
 @click.group()
@@ -29,4 +29,4 @@ install.add_command(prod)
 install.add_command(ssh_hardening)
 install.add_command(system)
 install.add_command(user)
-install.add_command(wizard)
+install.add_command(wizard_cmd)
